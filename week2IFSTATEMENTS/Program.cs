5f2bbf2a5cc60d11960b41ecc74e434a7cda2f59
deniz atmaca <deniz.atmaca@outlook.com>
2023-04-03 01:09:58 +0300

    week 2

---
+++ week2IFSTATEMENTS/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter your gender (f/m/n):");
+char userGender = Char.Parse(Console.ReadLine());
+
+string greeting;
+
+if(userGender == 'f')
+{
+    greeting = "Ms.";
+}
+else if (userGender ==  'm')
+{
+    greeting = "Mr.";
+}
+else 
+{
+    greeting = "";
+        }
+
+
+Console.WriteLine($"Hello, {greeting} {lastName}!");
+
+Console.WriteLine("Enter your year of bırth:");
+
+int usersYOB = Int32.Parse(Console.ReadLine());
+int currentYear = DateTime.Now.Year;
+int userAge = currentYear - usersYOB;
+Console.WriteLine($"Current year: {currentYear}");
+////int userAge = Int32.Parse(Console.ReadLine());
+
+if (userAge >= 13)
+{
+    Console.WriteLine($"You are {usersYOB} - old enough to have an account");
+}
+else 
+{
+    Console.WriteLine("You are {usersYOB} -too young to have an account");
+}
